bcafed1115047dd4da81dafe7ab9b84fbbc10d62
djbintley <33175390+djbintley@users.noreply.github.com>
2024-02-16 01:07:49 +0000

    bUZZING

--- Tasks/Task-152-Hysteresis/main.cpp
+++ Tasks/Task-152-Hysteresis/main.cpp
@@ -1,6 +1,7 @@
 #include "uop_msb.h"
 #include <chrono>
 #include <cstdint>
+#include <iostream>
 using namespace uop_msb;
 using namespace chrono;
 
@@ -13,8 +14,81 @@ DigitalOut greenLED(TRAF_GRN1_PIN);
 AnalogIn pot(AN_POT_PIN);
 Buzzer buzz;
 
+//BUZZ BUZZ BUZZ
+void coolBuzz(const char *noteStr)
+{
+    printf("%s\n",noteStr);
+    for(int i = 0;i < strlen(noteStr); i++){
+        switch (noteStr[i])
+                { 
+                    case 'A':
+                        if (noteStr[i+1]=='#') {
+                            buzz.playTone("A#");
+
+                            i++;
+                        }
+                        else buzz.playTone("A");
+                        break;
+                    case 'B':
+                        if (noteStr[i+1]=='#') {
+                            buzz.playTone("B#");
+                            i++;
+                        }
+                        else buzz.playTone("B");
+                        break;
+                    case 'C':
+                        if (noteStr[i+1]=='#') {
+                            buzz.playTone("C#");
+                            i++;
+                        }
+                        else buzz.playTone("C");
+                        break;
+                    case 'D':
+                        if (noteStr[i+1]=='#') {
+                            buzz.playTone("D#");
+                            i++;
+                        }
+                        else buzz.playTone("D");
+                        break;
+                    case 'E':
+                        if (noteStr[i+1]=='#') {
+                            buzz.playTone("E#");
+                            i++;
+                        }
+                        else buzz.playTone("E");
+                        break;
+                    case 'F':
+                        if (noteStr[i+1]=='#') {
+                            buzz.playTone("F#");
+                            i++;
+                        }
+                        else buzz.playTone("F");
+                        break;
+                    case 'G':
+                        if (noteStr[i+1]=='#') {
+                            buzz.playTone("G#");
+                            i++;
+                        }
+                        else buzz.playTone("G");
+                        break;
+                    default:
+                        buzz.rest();                  
+                        break;
+                }
+    wait_us(200000);
+    }
+    buzz.rest();
+}
+
+
+//BUZZ BUZZ BUZZ
+
 int main()
 {
+
+coolBuzz("AAAAAAFCAAFCAAAAEEEEEEFCAAFCAAAA");
+while(1){}
+
 int state = 0;
 unsigned short potVal   = pot.read_u16();
     if (potVal > 0x8000){
